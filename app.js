b02bb41a4a9de5790539fad12f3d030e91b9594a
Eldar <elogon@gmail.com>
2020-12-07 14:29:00 -0500

    Improve dialog text and add color to text using chalk

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const questions = require ('./lib/questions')
+const questions = require ('./lib/questions');
+const chalk = require('chalk'); // chalk npm package to output colored text into console
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -22,7 +23,7 @@ const askForTeamInfo = async () => {
             // if not then ask empplyee based questions
             askEmployeeInfo(type);
         } else {
-            console.log('Generating Team Web page ...');
+            console.log(chalk.yellow(`Generating Team's Web page ...`));
             const html = render(team);
             saveFile(html);
         }
@@ -32,12 +33,13 @@ const askForTeamInfo = async () => {
 
 // Function to ask more questions based on employee type
 const askEmployeeInfo = async (type) => {
+    console.log(chalk.yellow('-').repeat(60))
     return await inquirer.prompt(questions.employee)
     .then(employeeInfo => {
         inquirer.prompt(questions[type.role])
         .then(roleAnswer => {
             let allAnswers = {...employeeInfo, ...roleAnswer};
-            console.log(allAnswers);
+            console.log('\n');
             const { name, id, email, officeNumber, github, school} = allAnswers;
             switch (type.role) {
                 case 'manager':
@@ -58,34 +60,18 @@ const askEmployeeInfo = async (type) => {
     })
 }
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
+// Function to save generated html file to hard drive
 const saveFile = (html) => {
     // Check if output directory exist and create if it is not.
     if (!fs.existsSync(OUTPUT_DIR)){
         fs.mkdirSync(OUTPUT_DIR);
     }
     fs.writeFile(outputPath, html, (err) => 
-        err ? console.error(err) : console.log('Successfully saved team.html to output directory!')
+        err ? console.error(chalk.red(err)) : console.log(chalk.green('Successfully saved team.html to output directory!'))
     );
 }
 
+// Start application
+console.log(chalk.cyan('\n=== Welcome to ') + chalk.cyan.bold('Team Profile Summary ') + chalk.cyan('page generator! ===\n'));
 askForTeamInfo();
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-

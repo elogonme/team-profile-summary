e936457ce7c1a8c4933febbf5f6b867e4407b34d
Eldar <elogon@gmail.com>
2020-12-07 17:32:18 -0500

    Improve styling of template pages

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const path = require("path");
 const fs = require("fs");
 const questions = require ('./lib/questions');
 const chalk = require('chalk'); // chalk npm package to output colored text into console
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
